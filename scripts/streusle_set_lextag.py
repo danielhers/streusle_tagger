12f05ad0c24c5854d9ad7b554041d1bbefd5dfd0
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-06-17 17:04:55 +0200

    Use conllulex2json from streusle

--- scripts/streusle_set_lextag.py
+++ scripts/streusle_set_lextag.py
@@ -1,4 +1,8 @@
 import argparse
+import logging
+from streusle import conllulex2json
+
+logger = logging.getLogger(__name__)
 
 
 def main(args):
@@ -6,6 +10,9 @@ def main(args):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format="%(asctime)s - %(levelname)s "
+                        "- %(name)s - %(message)s",
+                        level=logging.INFO)
     argparser = argparse.ArgumentParser(description="Swap lextags into a STREUSLE file")
     argparser.add_argument("fname", help="conllulex or json file with full STREUSLE annotation")
     argparser.add_argument("lextags", help="jsonlines file: each line is a list of lextags for a sentence")
