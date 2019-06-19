10af1e232722a9b961f39cc6e5145a93892bd2dc
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-06-19 16:34:11 +0200

    Script to evaluate predictions for all STREUSLE models

---
+++ scripts/streuseval_all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+REF=$1
+shift
+for d in $*; do
+    IN=$d/predictions.txt
+    OUT=$d/`basename $REF .json`.autoid.json
+    [ -f $IN ] && python scripts/streusle_set_lextag.py $REF $IN > $OUT && echo $OUT
+done
+python -m streusle.streuseval streusle.ud_test.json models/*/streusle.ud_test.autoid.json
