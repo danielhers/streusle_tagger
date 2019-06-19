8c6499b3d46ecef207b2fead5f31f971d0b07e14
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-06-19 13:43:28 +0200

    Replace lextags in conllulex or json with predictions

--- scripts/streusle_set_lextag.py
+++ scripts/streusle_set_lextag.py
@@ -1,13 +1,22 @@
 import argparse
+import json
 import logging
-from streusle import conllulex2json
+
+from streusle.conllulex2json import load_sents, print_sents_json
 
 logger = logging.getLogger(__name__)
 
 
-def main(args):
-    pass
+def swap_lextags(sents, lextags_lines):
+    for sent, lextags_line in zip(sents, lextags_lines):
+        lextags = json.loads(lextags_line)
+        for tok, lextag in zip(sent["toks"], lextags):
+            tok["lextag"] = lextag
 
+
+def main(args):
+    with open(args.fname, encoding="utf-8") as f, open(args.lextags, encoding="utf-8") as lextags_lines:
+        print_sents_json(swap_lextags(load_sents(f), lextags_lines))
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s - %(levelname)s "
@@ -16,5 +25,4 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Swap lextags into a STREUSLE file")
     argparser.add_argument("fname", help="conllulex or json file with full STREUSLE annotation")
     argparser.add_argument("lextags", help="jsonlines file: each line is a list of lextags for a sentence")
-    argparser.add_argument("-o", "--out", help="output filename, default being derived from fname + lextags arguments")
     main(argparser.parse_args())
