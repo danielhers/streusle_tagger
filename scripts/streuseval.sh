b976e6d69274365e38b35f86a6cb4efc9cf8891f
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-08-12 17:28:21 +0200

    Fail on the first failing command

--- scripts/streuseval.sh
+++ scripts/streuseval.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+set -x
 
 REF="$1"
-
 shift
 for PRED in "$@"; do
     PREFIX="$(basename "$PRED" .jsonl)".
-    python scripts/streusle_set_lextag.py "$REF" "$PRED" > "$PREFIX"json
-    python -m json2conllulex "$PREFIX"json > "$PREFIX"conllulex
-    python -m conllulex2UDlextag "$PREFIX"conllulex > "$PREFIX"UDlextag
-    python -m UDlextag2json "$PREFIX"UDlextag > "$PREFIX"autoid.json
+    python scripts/streusle_set_lextag.py "$REF" "$PRED" > "$PREFIX"json || exit 1
+    python -m json2conllulex "$PREFIX"json > "$PREFIX"conllulex || exit 1
+    python -m conllulex2UDlextag "$PREFIX"conllulex > "$PREFIX"UDlextag || exit 1
+    python -m UDlextag2json "$PREFIX"UDlextag > "$PREFIX"autoid.json || exit 1
+    rm -f "$PREFIX"json "$PREFIX"conllulex "$PREFIX"UDlextag
 done
 python -m streuseval "$REF" *.autoid.json
