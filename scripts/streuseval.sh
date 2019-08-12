fd54a78222caa95bad5269dbf00044e72b8ef972
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-08-12 16:48:15 +0200

    Swap lextag predictions into json AND update the other fields for eval

--- scripts/streuseval.sh
+++ scripts/streuseval.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 REF="$1"
+
 shift
 for PRED in "$@"; do
-    python scripts/streusle_set_lextag.py "$REF" "$PRED" > "$(basename "$PRED" .jsonl)".autoid.json
+    PREFIX="$(basename "$PRED" .jsonl)".
+    python scripts/streusle_set_lextag.py "$REF" "$PRED" > "$PREFIX"json
+    python -m json2conllulex "$PREFIX"json > "$PREFIX"conllulex
+    python -m conllulex2UDlextag "$PREFIX"conllulex > "$PREFIX"UDlextag
+    python -m UDlextag2json "$PREFIX"UDlextag > "$PREFIX"autoid.json
 done
 python -m streuseval "$REF" *.autoid.json
